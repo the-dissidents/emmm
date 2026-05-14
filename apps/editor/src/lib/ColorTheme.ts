c1dad05befdde16898b1cef9e4a9403ab172256c
the-dissidents <the_dissidents@outlook.com>
2026-05-14 12:44:34 +0800

    feat: re-add 'automatically derive colors'

--- apps/editor/src/lib/ColorTheme.ts
+++ apps/editor/src/lib/ColorTheme.ts
@@ -1,14 +1,17 @@
-import * as Color from "colorjs.io/fn";
+import * as Color from 'colorjs.io/fn';
 
 export type ArticleColors = {
-    theme: Color.PlainColorObject,
-    text: Color.PlainColorObject,
-    commentary: Color.PlainColorObject,
-    link: Color.PlainColorObject,
-    highlight: Color.PlainColorObject
+    theme: Color.PlainColorObject;
+    text: Color.PlainColorObject;
+    commentary: Color.PlainColorObject;
+    link: Color.PlainColorObject;
+    highlight: Color.PlainColorObject;
 };
 
-export function getCssVariablesFromColors(t: ArticleColors, to: string | undefined = 'srgb') {
+export function getCssVariablesFromColors(
+    t: ArticleColors,
+    to: string | undefined = 'srgb'
+) {
     let f = to
         ? (x: Color.PlainColorObject) => Color.serialize(Color.toGamut(x, to))
         : (x: Color.PlainColorObject) => Color.serialize(x);
@@ -23,61 +26,104 @@ export function getCssVariablesFromColors(t: ArticleColors, to: string | undefin
     ]);
 }
 
-// export function deriveColorsFrom(theme: Color.PlainColorObject): ArticleColors {
-//     function f(x: number, a: number, b: number) {
-//         return Math.pow(x, a) * b;
-//     }
+export function deriveColorsFrom(theme: Color.PlainColorObject): ArticleColors {
+    function f(x: number, a: number, b: number) {
+        return Math.pow(x, a) * b;
+    }
 
-//     function invf(x: number, a: number, b: number) {
-//         return Math.pow(x, 1/a) * b + (1 - b);
-//     }
+    function invf(x: number, a: number, b: number) {
+        return Math.pow(x, 1 / a) * b + (1 - b);
+    }
 
-//     const themeHsl = Color.to(theme, 'hsl');
-//     if (themeHsl.coords[2] && themeHsl.coords[2] > 50) {
-//         let text = themeHsl;
-//         text.coords[1] = Math.pow((text.coords[1] ?? 0) / 100, 0.8) * 100;
-//         text.coords[2] = 10;
+    const themeHsl = Color.to(theme, 'hsl');
+    if (themeHsl.coords[2] && themeHsl.coords[2] > 50) {
+        const text = Color.clone(themeHsl);
+        text.coords[1] = Math.pow((text.coords[1] ?? 0) / 100, 0.8) * 100;
+        text.coords[2] = 10;
 
-//         let commentary = themeHsl;
-//         commentary.s = f((commentary.s ?? 0) / 100, 1.5, 0.6) * 100;
-//         commentary.l = f((commentary.l ?? 0) / 100, 0.6, 0.5) * 100;
-//         commentary.oklch.l = invf((text.oklch.l ?? 0), 1, 0.7);
+        const commentary = Color.clone(themeHsl);
+        commentary.coords[1] =
+            f((commentary.coords[1] ?? 0) / 100, 1.5, 0.6) * 100;
+        commentary.coords[2] =
+            f((commentary.coords[2] ?? 0) / 100, 0.6, 0.5) * 100;
+        const commentaryOklch = Color.to(commentary, 'oklch');
+        commentaryOklch.coords[0] = invf(
+            Color.to(text, 'oklch').coords[0] ?? 0,
+            1,
+            0.7
+        );
 
-//         let link = themeHsl;
-//         link.s = f((link.s ?? 0) / 100, 1, 0.6) * 100;
-//         link.l = f((link.l ?? 0) / 100, 0.5, 0.6) * 100;
-//         link.oklch.h = ((link.oklch.h ?? 0) + 20) % 360;
-//         link.oklch.l = invf((text.oklch.l ?? 0), 1, 0.6);
+        const link = Color.clone(themeHsl);
+        link.coords[1] = f((link.coords[1] ?? 0) / 100, 1, 0.6) * 100;
+        link.coords[2] = f((link.coords[2] ?? 0) / 100, 0.5, 0.6) * 100;
+        const linkOklch = Color.to(link, 'oklch');
+        linkOklch.coords[2] = ((linkOklch.coords[2] ?? 0) + 20) % 360;
+        linkOklch.coords[0] = invf(
+            Color.to(text, 'oklch').coords[0] ?? 0,
+            1,
+            0.6
+        );
 
-//         let highlight = theme.to('hsl');
-//         highlight.s = f((highlight.s ?? 0) / 100, 0.7, 1) * 100;
-//         highlight.l = f((highlight.l ?? 0) / 100, 1, 0.9) * 100;
-//         highlight.oklch.h = ((highlight.oklch.h ?? 0) - 25) % 360;
-//         highlight.oklch.l = f((theme.oklch.l ?? 0), 0.9, 0.9);
+        const highlight = Color.clone(themeHsl);
+        highlight.coords[1] = f((highlight.coords[1] ?? 0) / 100, 0.7, 1) * 100;
+        highlight.coords[2] = f((highlight.coords[2] ?? 0) / 100, 1, 0.9) * 100;
+        const highlightOklch = Color.to(highlight, 'oklch');
+        highlightOklch.coords[2] = ((highlightOklch.coords[2] ?? 0) - 25) % 360;
+        highlightOklch.coords[0] = f(
+            Color.to(theme, 'oklch').coords[0] ?? 0,
+            0.9,
+            0.9
+        );
 
-//         return {theme, text, commentary, link, highlight};
-//     } else {
-//         let text = theme.to('hsl');
-//         text.s = Math.pow((text.s ?? 0) / 100, 1.25) * 100;
-//         text.l = 95;
+        return {
+            theme,
+            text,
+            commentary: commentaryOklch,
+            link: linkOklch,
+            highlight: highlightOklch,
+        };
+    } else {
+        const text = Color.clone(themeHsl);
+        text.coords[1] = Math.pow((text.coords[1] ?? 0) / 100, 1.25) * 100;
+        text.coords[2] = 95;
 
-//         let commentary = theme.to('hsl');
-//         commentary.s = f((commentary.s ?? 0) / 100, 1.5, 0.6) * 100;
-//         commentary.l = invf((commentary.l ?? 0) / 100, 0.6, 0.5) * 100;
-//         commentary.oklch.l = f((text.oklch.l ?? 0), 1, 0.8);
+        const commentary = Color.clone(themeHsl);
+        commentary.coords[1] =
+            f((commentary.coords[1] ?? 0) / 100, 1.5, 0.6) * 100;
+        commentary.coords[2] =
+            invf((commentary.coords[2] ?? 0) / 100, 0.6, 0.5) * 100;
+        const commentaryOklch = Color.to(commentary, 'oklch');
+        commentaryOklch.coords[0] = f(
+            Color.to(text, 'oklch').coords[0] ?? 0,
+            1,
+            0.8
+        );
 
-//         let link = theme.to('hsl');
-//         link.s = f((link.s ?? 0) / 100, 1, 0.6) * 100;
-//         link.l = invf((link.l ?? 0) / 100, 0.5, 0.6) * 100;
-//         link.oklch.h = ((link.oklch.h ?? 0) + 20) % 360;
-//         link.oklch.l = f((text.oklch.l ?? 0), 1, 0.9);
+        const link = Color.clone(themeHsl);
+        link.coords[1] = f((link.coords[1] ?? 0) / 100, 1, 0.6) * 100;
+        link.coords[2] = invf((link.coords[2] ?? 0) / 100, 0.5, 0.6) * 100;
+        const linkOklch = Color.to(link, 'oklch');
+        linkOklch.coords[2] = ((linkOklch.coords[2] ?? 0) + 20) % 360;
+        linkOklch.coords[0] = f(Color.to(text, 'oklch').coords[0] ?? 0, 1, 0.9);
 
-//         let highlight = theme.to('hsl');
-//         highlight.s = f((highlight.s ?? 0) / 100, 0.7, 1) * 100;
-//         highlight.l = invf((highlight.l ?? 0) / 100, 1, 0.9) * 100;
-//         highlight.oklch.h = ((highlight.oklch.h ?? 0) - 25) % 360;
-//         highlight.oklch.l = invf((theme.oklch.l ?? 0), 0.9, 0.7);
+        const highlight = Color.clone(themeHsl);
+        highlight.coords[1] = f((highlight.coords[1] ?? 0) / 100, 0.7, 1) * 100;
+        highlight.coords[2] =
+            invf((highlight.coords[2] ?? 0) / 100, 1, 0.9) * 100;
+        const highlightOklch = Color.to(highlight, 'oklch');
+        highlightOklch.coords[2] = ((highlightOklch.coords[2] ?? 0) - 25) % 360;
+        highlightOklch.coords[0] = invf(
+            Color.to(theme, 'oklch').coords[0] ?? 0,
+            0.9,
+            0.7
+        );
 
-//         return {theme, text, commentary, link, highlight};
-//     }
-// }
+        return {
+            theme,
+            text,
+            commentary: commentaryOklch,
+            link: linkOklch,
+            highlight: highlightOklch,
+        };
+    }
+}
